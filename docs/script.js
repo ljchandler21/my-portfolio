cfe2553d432243ebdaabdfff5f5a22030dc4a0e9
ljchandler21 <loganjchandler@gmail.com>
2025-03-28 12:41:47 -0400

    re-add navbar, mod background

--- docs/script.js
+++ docs/script.js
@@ -3,11 +3,13 @@ document.addEventListener("DOMContentLoaded", function() {
     const navbar = document.getElementById("navbar");
 
     window.addEventListener("scroll", function() {
-        let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        let scrollTop = window.scrollY || document.documentElement.scrollTop;
         if (scrollTop > lastScrollTop) {
-            navbar.style.transform = "translateY(-100%)";
+            navbar.classList.add("hidden");
+            /*navbar.style.transform = "translateY(-100%)";*/
         } else {
-            navbar.style.transform = "translateY(0)";
+            navbar.classList.remove("hidden");
+            /*navbar.style.transform = "translateY(0)";*/
         }
         lastScrollTop = scrollTop;
     });
